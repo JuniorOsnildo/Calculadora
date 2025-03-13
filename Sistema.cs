17379236a9dc3b371c072d7a666007ef0a64488a
Osnildo <juniorosnildo777@gmail.com>
2025-03-13 20:15:46 -0300

    feito

--- Sistema.cs
+++ Sistema.cs
@@ -4,19 +4,23 @@ public class Sistema
 {
     public static void Somar(float numero1, float numero2)
     {
-        Console.WriteLine("Resultado: " + Math.Round(numero1 + numero2));
+        float resultado = numero1 + numero2;
+        Console.WriteLine("Resultado: " + Math.Round(resultado, 2));
     }
     public static void Sub(float numero1, float numero2)
     {
-        Console.WriteLine("Resultado: " + Math.Round(numero1 - numero2));
+        float resultado = numero1 - numero2;
+        Console.WriteLine("Resultado: " + Math.Round(resultado, 2));
     }
     public static void Mult(float numero1, float numero2)
     {
-        Console.WriteLine("Resultado: " + Math.Round(numero1 * numero2));
+        float resultado = numero1 * numero2;
+        Console.WriteLine("Resultado: " + Math.Round(resultado, 2));
     }
     public static void Div(float numero1, float numero2)
     {
-        Console.WriteLine("Resultado: " + Math.Round(numero1 / numero2));
+        float resultado = numero1 / numero2;
+        Console.WriteLine("Resultado: " + Math.Round(resultado, 2));
     }
     
 }
